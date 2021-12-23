340d7f47d06e97187a1f1404bc56ca37a48e7e63
Taeyang Jin <sun@jupiterflow.com>
2021-12-23 21:43:31 +0900

    Error Mapping 추가

--- pple-main-interfaces/src/main/kotlin/link/pple/main/interfaces/web/WebController.kt
+++ pple-main-interfaces/src/main/kotlin/link/pple/main/interfaces/web/WebController.kt
@@ -1,13 +1,14 @@
 package link.pple.main.interfaces.web
 
+import org.springframework.boot.web.servlet.error.ErrorController
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-class WebController {
+class WebController : ErrorController {
 
     @RequestMapping(
-        value = ["/"]
+        value = ["/", "/error"]
     )
     fun index(): String {
         return "generated/index.html"
