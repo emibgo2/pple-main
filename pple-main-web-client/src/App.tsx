486ef1e69985fb96226cb3b353473970987e9962
wook <gus5427@naver.com>
2021-12-25 02:22:49 +0900

    Rename : rename path

--- pple-main-web-client/src/App.tsx
+++ pple-main-web-client/src/App.tsx
@@ -4,7 +4,7 @@ import reset from 'styled-reset';
 import styled from 'styled-components';
 import NavigationBar from './components/navigation/NavigationBar';
 import GlobalNavigationBar from './components/navigation/GlobalNavigationBar';
-import {Routes,Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
@@ -34,13 +34,12 @@ const App: React.FC = () => {
         <ContentBlock>
           <GlobalNavigationBar />
           <Routes>
-            <Route path="/" element={<HomePage/>}/>
-            <Route path="/login" element={<LoginPage/>}/>
-            <Route path="/register" element={<RegisterPage/>}/>
-            <Route path="/post" element={<RequestPostPage/>}/>
-            <Route path="/post/register" element={<RequestRegisterPage/>}/>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/post" element={<RequestPostPage />} />
+            <Route path="/postregister" element={<RequestRegisterPage />} />
           </Routes>
-
         </ContentBlock>
       </AppBlock>
     </>
