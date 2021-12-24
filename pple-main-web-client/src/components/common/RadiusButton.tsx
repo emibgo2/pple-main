602bf7e54ea326e4deb3e63fa0c29e64c0169955
wook <gus5427@naver.com>
2021-12-25 02:08:54 +0900

    Design : add RequestRegister Component

--- pple-main-web-client/src/components/common/RadiusButton.tsx
+++ pple-main-web-client/src/components/common/RadiusButton.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledButton = styled.button`
-  /* display: flex;
-  flex-direction: row;
-  align-items: flex-start; */
   background: #767676;
   border-radius: 20px;
   padding: 6px 9px;
@@ -12,11 +9,11 @@ const StyledButton = styled.button`
 `;
 
 type Props = {
-    text : string,
-    isDisabled? : boolean
+    text: string;
+    isDisabled?: boolean;
 }
 
-const RadiusButton:React.FC<Props> = ({text,isDisabled}) => {
+const RadiusButton: React.FC<Props> = ({text,isDisabled}) => {
   return <StyledButton disabled={isDisabled}>{text}</StyledButton>;
 };
 
