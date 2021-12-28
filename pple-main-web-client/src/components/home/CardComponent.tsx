4ed75db4ee68bd55886ee94911d6a7a9951774bc
nookcoder <gus5427@naver.com>
2021-12-28 22:56:27 +0900

    Design : change cardcomponent design

--- pple-main-web-client/src/components/home/CardComponent.tsx
+++ pple-main-web-client/src/components/home/CardComponent.tsx
@@ -1,137 +1,91 @@
 import React from 'react';
 import styled from 'styled-components';
-import palette from '../../lib/styles/palette';
-import BloodMark from '../common/BloodMark';
-import Button from '@mui/material/Button'
-import Divider from '../common/Divider';
+import heartImg from '../../lib/images/heart.png';
+import bar from '../../lib/images/bar.png';
+import ChattingButton from '../common/ChattingButton';
 
 const CardComponentBlock = styled.div`
-  width: 25.75rem;
-  height: 17.188rem;
-  background-color: white;
-  box-shadow: 3px 3px 20px rgba(73, 0, 0, 0.05);
-  border-radius: 20px;
+  width: fit-content;
+  border-radius: 14px;
+  border:1px solid black;
+  & .header {
+    background-color: whitesmoke;
+  }
 `;
 
-const RegistrationDate = styled.div`
+const TimeBox = styled.div`
+padding:4px;
   font-size: x-small;
-  opacity: 0.3;
-  font-weight: bolder;
-  text-align: right;
-  padding: 15px 20px 0px 0px;
-`;
-
-const CardHeaderBox = styled.div`
   width: 100%;
-  box-sizing: border-box;
-  padding: 0px 20px 20px 20px;
-  height: max-content;
-  background: ${palette.gray[0]};
-  border-radius: 1.5rem 1.5rem 0 0;
+  text-align: right;
 `;
 
-const CardHeaderContentBox = styled.div`
+const CardComponentHeader = styled.div`
   display: flex;
   align-items: center;
-  padding-top: 10px;
-`;
-
-const CardHeaderImageBox = styled.div`
-  width: fit-content;
-  margin-right: 15px;
-`;
-
-const CardHeaderTextBox = styled.div`
-  width: 100%;
-  height: 100%;
-  padding-top: 30px;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const CardHeaderText = styled.div`
-  & > h3 {
-    font-size: small;
-    font-weight: bolder;
-    margin-bottom: 2px;
-  }
-
-  & > p {
-    font-size: x-small;
-    font-weight: bold;
-    opacity: 0.5;
-  }
-`;
-
-const CardContentBox = styled.div`
-    padding:20px 30px;
-    & > h2 {
-        font-weight:bolder;
-        font-size:large;
-        margin-bottom: 3px;
-    }
-    & > span{
-        font-size:x-small;
-    }
-`;
-
-const ButtonGroup = styled.div`
+  justify-content: center;
+  & .middle {
     display: flex;
-    align-items:center;
-    justify-content:space-between;
-    margin-top:10px;
+    align-items: center;
+    justify-content: center;
+  }
+  & .middle .header-text .header-text-title {
+    color: gray;
+    font-size: smaller;
+  }
 `;
 
-const CardComponent = () => {
+const CardComponentHeaderColumn = styled.div`
+  & .blood-icon {
+    position: relative;
+  }
+  & .blood-icon .card-text {
+    position: absolute;
+    left: 25%;
+    bottom: 35%;
+    color: white;
+    font-weight: bold;
+  }
+`;
 
-    const messagStyle = { 
-        width:"48%", 
-        color : palette.red[1], 
-        borderColor : palette.red[1],
-        borderRadius: "20px"
-    }
-    const postStyle = {
-        width:"48%",
-        backgroundColor:palette.red[1],
-        borderRadius: "20px"
+const CardComponentContent = styled.div``;
 
-    }
+interface CardProps {
+  bloodType: string;
+  sort: string;
+  story: string;
+}
+
+const CardComponent: React.FC<CardProps> = ({ bloodType, sort, story }) => {
   return (
     <CardComponentBlock>
-      <CardHeaderBox>
-        <RegistrationDate>2021년 12월 13일 오전 7:30</RegistrationDate>
-        <CardHeaderContentBox>
-          <CardHeaderImageBox>
-            <BloodMark type="b+" />
-          </CardHeaderImageBox>
-          <CardHeaderTextBox>
-            <CardHeaderText>
-              <h3>윤성호</h3>
-              <p>520221(생년월일)</p>
-            </CardHeaderText>
-            <Divider />
-            <CardHeaderText>
-              <h3>혈소판성분채혈</h3>
-              <p>헌혈 종류</p>
-            </CardHeaderText>
-            <Divider />
-            <CardHeaderText>
-              <h3>3회</h3>
-              <p>남은 횟수</p>
-            </CardHeaderText>
-          </CardHeaderTextBox>
-        </CardHeaderContentBox>
-      </CardHeaderBox>
+      <div className="header">
+        <TimeBox><div>2021년 12월 13일 등록</div></TimeBox>
+        <CardComponentHeader>
+          <CardComponentHeaderColumn>
+            <div className="blood-icon">
+              <div className="card-img">
+                <img src={heartImg} alt="하트이미지" />
+              </div>
+              <div className="card-text">{bloodType}</div>
+            </div>
+          </CardComponentHeaderColumn>
 
-      <CardContentBox>
-          <h2>너무 급합니다 ㅠㅠ</h2>
-          <span>지난 11월 27일에 저희 아버지께서 급성 골수성 백혈병 (M3)를 판정받아 지난주부터 항암치료에 들어가셨습니다. 이 유형의 백혈병이 치료 과정에 혈소판을  ... </span>
-          <span>더보기</span>
-          <ButtonGroup>
-              <Button sx={messagStyle} variant="outlined" color="error" size="large">메세지 보내기</Button>
-              <Button sx={postStyle} variant="contained" color="error" size="large">사연 더보기</Button>
-          </ButtonGroup>
-      </CardContentBox>
+          <CardComponentHeaderColumn className="middle">
+            <div>
+              <img src={bar} alt="체온계이미지"></img>
+            </div>
+            <div className="header-text">
+              <div className="header-text-title">헌혈 종류</div>
+              <div>{sort}</div>
+            </div>
+          </CardComponentHeaderColumn>
+          <CardComponentHeaderColumn>
+            <ChattingButton />
+          </CardComponentHeaderColumn>
+        </CardComponentHeader>
+      </div>
+      <CardComponentContent>{story}</CardComponentContent>
     </CardComponentBlock>
   );
 };
