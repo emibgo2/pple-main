b126f696e8f4d5f2e922e439adbbf4a811979994
Hyeonuook Kim <79124461+nookcoder@users.noreply.github.com>
2022-01-10 11:45:34 +0900

    Delete src directory

--- src/lib/customAxios.ts
+++
@@ -1,7 +0,0 @@
-import axios, { AxiosInstance } from 'axios';
-
-export const customAxios: AxiosInstance = axios.create({
-  baseURL: `http://ppledev.link`, // 기본 서버 주소 입력
-  withCredentials : true, 
-});
-
