c8b1c8886f0f8c14b5b7f43a8af7df8ea3f0ef54
wook <gus5427@naver.com>
2022-01-18 00:24:46 +0900

    Feat : add Token 값이 '' 일 때 로그인 화면으로 이동

--- pple-main-web-client/src/pages/MyPage.tsx
+++ pple-main-web-client/src/pages/MyPage.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import LoginRequestModal from '../components/common/modal/LoginRequestModal';
 import MyPageHeader from '../components/mypage/MyPageHeader';
 import MyPageList from '../components/mypage/MyPageList';
 import MyPageTemplate from '../components/mypage/MyPageTemplate';
 import Profile from '../components/mypage/Profile';
+import { RootState } from '../models';
 const MyPage: React.FC = () => {
+    const token = useSelector((state: RootState) => state.account.token); 
+    const [open, setOpen] = useState(false); 
+    const handleModalOpen = () =>{
+      setOpen(!open); 
+    };
+    useEffect(()=>{
+      if(token == ''){
+        setOpen(true);
+      }
+    })
     return (
       <MyPageTemplate>
-        <MyPageHeader/>
+        <LoginRequestModal open={open} onClick={handleModalOpen} />
+        <MyPageHeader/> 
         <Profile />
         <MyPageList />
       </MyPageTemplate>
