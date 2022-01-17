cb3485cdd963cf957e104fec25bf90e1942b55e7
wook <gus5427@naver.com>
2022-01-18 00:47:46 +0900

    Feat : get UserToken

--- pple-main-web-client/src/pages/HomePage.tsx
+++ pple-main-web-client/src/pages/HomePage.tsx
@@ -8,54 +8,57 @@ import { useNavigate, useSearchParams } from 'react-router-dom';
 import { customAxios } from '../lib/customAxios';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../models';
-import { setToken } from '../models/auth/token';
-import { setUuid } from '../models/auth/uuid';
+import { setToken } from '../models/auth/account';
 
 const HomepageBlock = styled.div``;
 
 const HomePage: React.FC = () => {
-    // const navigate = useNavigate();
-    // const [searchParams, setSearchParams] = useSearchParams();
-    // const token = useSelector((state: RootState) => state.setToken.token);
-    // const dispatch = useDispatch();
+  // const navigate = useNavigate();
+  // const token = useSelector((state: RootState) => state.setToken.token);
+  // const dispatch = useDispatch();
 
-    // useEffect(() => {
-    //   const userToken = searchParams.get('token');
-    //   dispatch(setToken(userToken));
-    //   console.log('userToken', userToken);
+  // useEffect(() => {
+  //   const userToken = searchParams.get('token');
+  //   dispatch(setToken(userToken));
+  //   console.log('userToken', userToken);
 
-    //   if (userToken !== null) {
-    //     customAxios.defaults.headers.common[
-    //       'Authorization'
-    //     ] = `Bearer ${userToken}`;
-    //   }
-    //   customAxios.get('/api/v1/account/profile').then(res => {
-    //     console.log(res.data.status);
-    //     dispatch(setUuid(res.data.uuid));
-    //     if (res.data.status == 'TEMP') {
-    //       navigate('/register');
-    //     }
-    //   });
+  //   if (userToken !== null) {
+  //     customAxios.defaults.headers.common[
+  //       'Authorization'
+  //     ] = `Bearer ${userToken}`;
+  //   }
+  //   customAxios.get('/api/v1/account/profile').then(res => {
+  //     console.log(res.data.status);
+  //     dispatch(setUuid(res.data.uuid));
+  //     if (res.data.status == 'TEMP') {
+  //       navigate('/register');
+  //     }
+  //   });
 
-    //   axios.get('http://ppledev.link/api/v1/account/profile').then(res => {
-    //     console.log(res.data);
-    //     // 전역으로 현재 로그인된 계정 상태 관리
-    //     if (res.data.status == 'TEMP') {
-    //       navigate('/register');
-    //     }
-    //   });
-    // });
-    const uuid = useSelector((state: RootState) => state.account?.uuid); 
-    const token = useSelector((state: RootState) => state.account?.token);
-    const dispatch = useDispatch(); 
-    
+  //   axios.get('http://ppledev.link/api/v1/account/profile').then(res => {
+  //     console.log(res.data);
+  //     // 전역으로 현재 로그인된 계정 상태 관리
+  //     if (res.data.status == 'TEMP') {
+  //       navigate('/register');
+  //     }
+  //   });
+  // });
 
-    return (
-        <HomepageBlock>
-            <HomePageHeader name="지니"/>
-            <CardTemplate/>
-        </HomepageBlock>
-    );
+  const uuid = useSelector((state: RootState) => state.account?.uuid);
+  const token = useSelector((state: RootState) => state.account?.token);
+  const dispatch = useDispatch();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const userToken = searchParams.get('token'); 
+  useEffect(()=>{
+      console.log(userToken);
+  });
+
+  return (
+    <HomepageBlock>
+      <HomePageHeader name="지니" />
+      <CardTemplate />
+    </HomepageBlock>
+  );
 };
 
 export default HomePage;
