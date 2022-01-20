993345f928b0af3c5cb7b1389a6fe373fbe0dd38
wook <gus5427@naver.com>
2022-01-20 23:53:36 +0900

    Feat : check cookies and status

--- pple-main-web-client/src/pages/HomePage.tsx
+++ pple-main-web-client/src/pages/HomePage.tsx
@@ -2,22 +2,13 @@ import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import HomePageHeader from '../components/home/HomePageHeader';
 import CardTemplate from '../components/home/CardTemplate';
-import { useSearchParams } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../models';
-import { setToken } from '../models/auth/account';
-import { customAxios } from '../lib/customAxios';
-import setAccessToken from '../lib/hooks/setAccessToken';
-import checkUserState from '../lib/hooks/checkUserState';
-
+import { setCookie } from '../lib/hooks/CookieUtil';
+import { checkUser } from '../lib/hooks/CookieUtil';
 const HomepageBlock = styled.div``;
 
 const HomePage: React.FC = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const userToken = searchParams.get('token');
-  setAccessToken(userToken);
-  checkUserState();
-
+  setCookie();
+  checkUser();
   return (
     <HomepageBlock>
       <HomePageHeader name="지니" />
