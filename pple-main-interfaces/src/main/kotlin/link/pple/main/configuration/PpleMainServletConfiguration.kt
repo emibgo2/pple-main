0a4552f9368e0a0e83c47187856ecae998cf3743
Taeyang Jin <sun@jupiterflow.com>
2022-01-29 12:44:02 +0900

    feat: Favicon render by springboot

--- pple-main-interfaces/src/main/kotlin/link/pple/main/configuration/PpleMainServletConfiguration.kt
+++ pple-main-interfaces/src/main/kotlin/link/pple/main/configuration/PpleMainServletConfiguration.kt
@@ -3,6 +3,7 @@ package link.pple.main.configuration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.ClassPathResource
+import org.springframework.web.servlet.function.EntityResponse.fromObject
 import org.springframework.web.servlet.function.router
 
 @Configuration
@@ -10,7 +11,7 @@ class PpleMainServletConfiguration {
 
     @Bean
     fun staticResourcesRouter() = router {
-//        "/favicon.ico" { fromObject(ClassPathResource("/static/generated/resources/favicon.ico")).build() }
+        "/favicon.ico" { fromObject(ClassPathResource("/static/generated/logo.png")).build() }
         resources("/static/**", ClassPathResource("/static/generated/static/"))
     }
 }
