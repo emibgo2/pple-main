8c1fe671051d1d9d0e94c708d0adfdea59ca2df8
wook <gus5427@naver.com>
2022-01-31 12:38:26 +0900

    Feat : sorting posting by value

--- pple-main-web-client/src/components/home/CardComponent.tsx
+++ pple-main-web-client/src/components/home/CardComponent.tsx
@@ -15,6 +15,22 @@ const CardContentBox = styled('div')({
   padding: '8px 27px',
 });
 
+const ContentWithMore = styled('div')({
+  fontStyle: 'normal',
+  fontWeight: 'bold',
+  fontSize: '18px',
+  lineHeight: '22px',
+  letterSpacing: '-0.03em',
+  color: '#222222',
+  margin: '17px 0px',
+  padding: '0px 27px 17px 27px',
+  '& button': {
+    background: 'none',
+    border: 'none',
+    cursor: 'pointer',
+  },
+});
+
 interface Props {
   title: string;
   content: string;
@@ -32,9 +48,13 @@ const CardComponent: React.FC<Props> = ({
   time,
   phoneNumber,
 }) => {
+  const [detail, setDetail] = useState(false);
   const [open, setOpen] = useState(false);
   const handleOpen = (open: boolean) => {
     setOpen(!open);
+  };
+  const handleDetail = () => {
+    setDetail(!detail);
   };
 
   const onClick = () => {
@@ -52,9 +72,16 @@ const CardComponent: React.FC<Props> = ({
           time={time}
           onClick={onClick}
         />
-        <CardContentBox>
-          <FeedContent title={title} content={content} />
-        </CardContentBox>
+        {detail ? (
+          <CardContentBox>
+            <FeedContent title={title} content={content} />
+          </CardContentBox>
+        ) : (
+          <ContentWithMore>
+            {title.slice(0, 22)}
+            <button onClick={handleDetail}>...더보기</button>
+          </ContentWithMore>
+        )}
       </Paper>
       <ConnectionModal
         handleOpen={handleOpen}
--- pple-main-web-client/src/components/home/CardTemplate.tsx
+++ pple-main-web-client/src/components/home/CardTemplate.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from 'styled-components';
 import { Button, styled } from '@mui/material';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
@@ -42,20 +42,136 @@ const StyledButton = styled(Button)({
   },
 });
 
+const RenderPost = (contentArray: any) => {
+  return contentArray.map((content, idx) => (
+    <CardComponent
+      key={content.uuid}
+      title={content.title}
+      content={content.content}
+      sort={content.bloodProduct[0]}
+      bloodType={
+        content.patient.blood.rh == 'POSITIVE'
+          ? `${content.patient.blood.abo}+`
+          : `${content.patient.blood.abo}-`
+      }
+      time={content.createdAt}
+      phoneNumber={content.phoneNumber}
+    />
+  ));
+};
+
+const FilterBloodType = (bloodType: string, contentArray: any) => {
+  return contentArray.map((content, idx) =>
+    content.patient.blood.abo == bloodType.replace('형', '') ? (
+      <CardComponent
+        key={content.uuid}
+        title={content.title}
+        content={content.content}
+        sort={content.bloodProduct[0]}
+        bloodType={
+          content.patient.blood.rh == 'POSITIVE'
+            ? `${content.patient.blood.abo}+`
+            : `${content.patient.blood.abo}-`
+        }
+        time={content.createdAt}
+        phoneNumber={content.phoneNumber}
+      />
+    ) : (
+      <div key={idx}></div>
+    ),
+  );
+};
+
+const ChangeBloodProductValue = (bloodProduct: string) => {
+  switch (bloodProduct) {
+    case '전혈':
+      return 'WHOLE';
+    case '성분채혈 혈소판':
+      return 'PLATELET';
+    case '성분채혈 백혈구':
+      return 'LEUKOCYTE';
+    case '농축적혈구':
+      return 'PACKED_RED_BLOOD_CELL';
+    case '백혈구여과제거적혈구':
+      return 'LEUKOCYTE_REDUCED_RED_BLOOD_CELL';
+    default:
+      break;
+  }
+};
+
+const FilterBloodProduct = (bloodProduct: string, contentArray: any) => {
+  return contentArray.map((content, idx) =>
+    content.bloodProduct[0] == ChangeBloodProductValue(bloodProduct) ? (
+      <CardComponent
+        key={content.uuid}
+        title={content.title}
+        content={content.content}
+        sort={content.bloodProduct[0]}
+        bloodType={
+          content.patient.blood.rh == 'POSITIVE'
+            ? `${content.patient.blood.abo}+`
+            : `${content.patient.blood.abo}-`
+        }
+        time={content.createdAt}
+        phoneNumber={content.phoneNumber}
+      />
+    ) : (
+      <div key={idx}></div>
+    ),
+  );
+};
+
+const FilterBloodTypeAndBloodProduct = (
+  bloodType: string,
+  bloodProduct: string,
+  contentArray,
+) => {
+  return contentArray.map((content, idx) =>
+    content.patient.blood.abo == bloodType.replace('형', '') &&
+    content.bloodProduct[0] == ChangeBloodProductValue(bloodProduct) ? (
+      <CardComponent
+        key={idx}
+        title={content.title}
+        content={content.content}
+        sort={content.bloodProduct[0]}
+        bloodType={
+          content.patient.blood.rh == 'POSITIVE'
+            ? `${content.patient.blood.abo}+`
+            : `${content.patient.blood.abo}-`
+        }
+        time={content.createdAt}
+        phoneNumber={content.phoneNumber}
+      />
+    ) : (
+      <div key={idx}></div>
+    ),
+  );
+};
+
 type CardTemplateType = {
   handleAuth: any;
   contentArray: any;
+};
+
+export type FilterType = {
+  bloodType: string;
+  bloodProduct: string;
 };
 
 const CardTemplate: React.FC<CardTemplateType> = ({
   handleAuth,
   contentArray,
 }) => {
+  const [filter, setFilter] = useState<FilterType>({
+    bloodType: null,
+    bloodProduct: null,
+  });
+
   return (
     <CardContainerBlock>
       <ButtonGroup>
         <div className="sort">
-          <SortingButtonGroup />
+          <SortingButtonGroup filter={filter} setFilter={setFilter} />
         </div>
         <div>
           <StyledButton sx={{ padding: '0px' }} onClick={handleAuth}>
@@ -64,21 +180,17 @@ const CardTemplate: React.FC<CardTemplateType> = ({
           </StyledButton>
         </div>
       </ButtonGroup>
-      {contentArray.map((content, idx) => (
-        <CardComponent
-          key={idx}
-          title={content.title}
-          content={content.content}
-          sort={content.bloodProduct[0]}
-          bloodType={
-            content.patient.blood.rh == 'POSITIVE'
-              ? `${content.patient.blood.abo}+`
-              : `${content.patient.blood.abo}-`
-          }
-          time={content.createdAt}
-          phoneNumber={content.phoneNumber}
-        />
-      ))}
+      {filter.bloodProduct && filter.bloodType
+        ? FilterBloodTypeAndBloodProduct(
+            filter.bloodType,
+            filter.bloodProduct,
+            contentArray,
+          )
+        : filter.bloodProduct
+        ? FilterBloodProduct(filter.bloodProduct, contentArray)
+        : filter.bloodType
+        ? FilterBloodType(filter.bloodType, contentArray)
+        : RenderPost(contentArray)}
     </CardContainerBlock>
   );
 };
